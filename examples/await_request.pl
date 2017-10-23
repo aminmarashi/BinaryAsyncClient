4f91502ebb84d778af5953dd67fa27c1ddfdf2cf
Amin Marashi <marashi.s.amin@gmail.com>
2017-10-23 07:49:37 +0000

    Add await_request example

---
+++ examples/await_request.pl
@@ -0,0 +1,30 @@
+use strict;
+use warnings;
+
+use BinaryAsync::Client;
+use IO::Async::Loop;
+use Data::Dumper;
+
+my $loop = IO::Async::Loop->new;
+my $ws_client = BinaryAsync::Client->new(uri => 'wss://ws.binaryws.com/websockets/v3?l=EN&app_id=1');
+
+$loop->add($ws_client);
+
+my $req = {
+    proposal      => 1,
+    amount        => 100,
+    basis         => 'payout',
+    currency      => 'USD',
+    contract_type => 'CALL',
+    symbol        => 'frxAUDJPY',
+    duration      => 3,
+    duration_unit => 'm'
+};
+
+
+my $proposal = $ws_client->await_request($req);
+
+warn Dumper $proposal;
+
+1;
+
